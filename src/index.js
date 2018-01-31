c51454e051bd893fb32895b31de92bb54e3123da
Kristian Angelski <kristian.angelski@gmail.com>
2018-01-31 03:06:33 +0200

    all warnings that were produced by eslint are now cleaned

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const bookValue = numeral(500).format('$0,0.00');
-console.log(`I would pay ${bookValue} for this awesome book!`);
+console.log(`I would pay ${bookValue} for this awesome book!`); // eslint-disable-line no-console
