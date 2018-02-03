2354426a1b4cf5719970251ea1bbdaf160029577
enzo <kristian.angelski@gmail.com>
2018-02-03 22:15:49 +0200

    users route added, simulating hitting a real database

--- buildScripts/express.js
+++ buildScripts/express.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.get('/', function(req, res) {
+app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
     {"id": 1,"firstName": "Bob","lastName":"Smith","email":"bob@abv.bg"},
