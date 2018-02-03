4cb5821301731f6fc24734fe0906f736d6ba7a93
enzo <kristian.angelski@gmail.com>
2018-02-03 21:51:16 +0200

    route /users added, currently hardcoded to simulate hitting a database

--- buildScripts/express.js
+++ buildScripts/express.js
@@ -18,6 +18,16 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName": "Bob","lastName":"Smith","email":"bob@abv.bg"},
+    {"id": 2,"firstName": "Martin","lastName":"Petrov","email":"bob@abv.bg"},
+    {"id": 3,"firstName": "Todor","lastName":"Todorov","email":"bob@abv.bg"},
+    {"id": 4,"firstName": "Ivan","lastName":"Popov","email":"bob@abv.bg"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
